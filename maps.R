85b722dc6953ac5beae139f0d3cc348b91c2ab2f
Franz M. <econaut@users.noreply.github.com>
2015-09-28 16:25:59 +0200

    Update maps.R

--- maps.R
+++ maps.R
@@ -1,24 +1,28 @@
-maps<-function(object,mode="driving"){
-  library(rjson)
-  n<-nrow(object)
-  dist<-c()
-  dur<-c()
-  for (i in 1:n){
-    if (mode=="transit"){
-      u<-paste("http://maps.googleapis.com/maps/api/directions/json?origin=",object[i,1],
-               "&destination=",object[i,2],
-               "&mode=transit","&arrival_time=21600",sep="")
-    }
-    else {
-      u<-paste("http://maps.googleapis.com/maps/api/directions/json?origin=",object[i,1],
-               "&destination=",object[i,2],sep="")}
-    if (inherits(try(dat<-fromJSON(file=file(u))), "try-error")){next}
-    else {
-      dist<-append(dist,as.numeric(dat[[2]][[1]][["legs"]][[1]][["distance"]][2])/1000)
-      dur<-append(dur,as.numeric(dat[[2]][[1]][["legs"]][[1]][["duration"]][2])/60)
-    }
-    Sys.sleep(.5)
+maps<-function(object,mode="driving",arrival="21600",auth=NULL,region=NULL){
+  goog <- "https://maps.googleapis.com/maps/api/distancematrix/json?origins="
+  from <- paste(levels(object[,1]),collapse="|")
+  to <- paste(levels(object[,2]),collapse="|")
+  
+  mod<-paste("&mode=",mode,sep="")
+  arriv<-paste("&arrival_time=",arrival,sep="")
+  aut<-paste("&key",auth,sep="")
+  reg<-paste("&region=",region,sep="")
+  
+  u <- paste(goog,from,"&destinations=",to,sep="")
+  if(mode=="transit") u<-paste(u,mod,arriv,sep="")
+  if(!is.null(region)) u<-paste(u,reg,sep="")
+  if(!is.null(auth)) u<-paste(u,aut,sep="")
+  f <- file(u)
+  dat <- fromJSON(file=f)
+  
+  dist <- c()
+  dur <- c()
+  for (i in 1:nrow(object)){
+    dist <- append(dist,dat$rows[[i]]$elements[[1]]$distance$value/1000) # extract kilometers
+    dur <- append(dur,dat$rows[[i]]$elements[[1]]$duration$value/3600) # extract hours of journey
   }
-  result<-data.frame(object,data.frame("distance"=dist,"duration"=dur))
-  return(result)
+  close(f)
+
+  res <- cbind(object, data.frame("distance"=dist,"duration"=dur))
+  return(res)
 }
