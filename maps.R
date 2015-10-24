76cae3d422ca097fcc59e0f2fe4be172fd19286d
Franz M <econaut@users.noreply.github.com>
2015-10-24 19:36:59 +0200

    Update maps.R

--- maps.R
+++ maps.R
@@ -1,8 +1,7 @@
 maps<-function(object,mode="driving",arrival="21600",auth=NULL,region=NULL){
   goog <- "https://maps.googleapis.com/maps/api/distancematrix/json?origins="
-  from <- paste(levels(object[,1]),collapse="|")
-  to <- paste(levels(object[,2]),collapse="|")
-  
+  from <- paste(object[,1],collapse="|")
+  to <- paste(object[,2],collapse="|")
   mod<-paste("&mode=",mode,sep="")
   arriv<-paste("&arrival_time=",arrival,sep="")
   aut<-paste("&key",auth,sep="")
@@ -12,14 +11,19 @@ maps<-function(object,mode="driving",arrival="21600",auth=NULL,region=NULL){
   if(mode=="transit") u<-paste(u,mod,arriv,sep="")
   if(!is.null(region)) u<-paste(u,reg,sep="")
   if(!is.null(auth)) u<-paste(u,aut,sep="")
+  
+  if (length(strsplit(u,split="")[[1]])>1950) {
+    stop(gettext("Number of directions too large."))
+  }
+  
   f <- file(u)
   dat <- fromJSON(file=f)
   
   dist <- c()
   dur <- c()
   for (i in 1:nrow(object)){
-    dist <- append(dist,dat$rows[[i]]$elements[[1]]$distance$value/1000) # extract kilometers
-    dur <- append(dur,dat$rows[[i]]$elements[[1]]$duration$value/3600) # extract hours of journey
+    dist <- append(dist,dat$rows[[i]]$elements[[1]]$distance$value/1000)
+    dur <- append(dur,dat$rows[[i]]$elements[[1]]$duration$value/3600)
   }
   close(f)
 
